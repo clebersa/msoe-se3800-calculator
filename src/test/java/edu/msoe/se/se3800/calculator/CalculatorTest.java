b4ac696bbe19ebb4fccf15daa9d05c74c3d3108d
Cleber de Souza Alcântara <cleber.93cd@gmail.com>
2016-01-13 22:33:43 -0600

    - Removing tests of the class Main that were copied to CalculatorTest. - Commenting some tests methods in CalculatorTest. - Removing useless dependence from pom.xml.

--- src/test/java/edu/msoe/se/se3800/calculator/CalculatorTest.java
+++ src/test/java/edu/msoe/se/se3800/calculator/CalculatorTest.java
@@ -23,11 +23,6 @@ public class CalculatorTest {
      */
     private ByteArrayOutputStream sysout;
 
-    /**
-     * Input stream used to insert information in the console.
-     */
-    private ByteArrayInputStream sysin;
-
     @Before
     public void setUp() throws UnsupportedEncodingException {
         sysout = new ByteArrayOutputStream();
@@ -39,36 +34,66 @@ public class CalculatorTest {
         System.setOut(System.out);
     }
 
+    /**
+     * Test pair of parenthesis.
+     *
+     * @author cleber
+     */
     @Test(expected = InvalidParameterException.class)
     public void testParseEquationParen6() {
         Calculator instance = new Calculator();
         instance.solveEquation("add 3 (add 4 5 )");
     }
 
+    /**
+     * Tests invalid '('
+     *
+     * @author cleber
+     */
     @Test(expected = InvalidParameterException.class)
     public void testParseEquationParen7() {
         Calculator instance = new Calculator();
         instance.solveEquation("add 3 (add 4 ( 5)");
     }
 
+    /**
+     * Tests pair of parenthesis not nestled.
+     *
+     * @author cleber
+     */
     @Test
     public void testParseEquationParen8() {
         Calculator instance = new Calculator();
         assertEquals(30, instance.solveEquation("add 1 2 (add 10 20) 3 4 (sub 30 40)"), 0);
     }
 
+    /**
+     * Tests nestled parenthesis.
+     *
+     * @author cleber
+     */
     @Test
     public void testParseEquationParen10() {
         Calculator instance = new Calculator();
         assertEquals(20033, instance.solveEquation("add 1 2 (add 10 20 (mul 100 200))"), 0);
     }
 
+    /**
+     * Tests invalid element between ')'s.
+     *
+     * @author cleber
+     */
     @Test(expected = InvalidParameterException.class)
     public void testParseEquationParen11() {
         Calculator instance = new Calculator();
         instance.solveEquation("add 1 2 (add 10 20 (mul 100 200)?)");
     }
 
+    /**
+     * Tests unexpected '('.
+     *
+     * @author cleber
+     */
     @Test(expected = InvalidParameterException.class)
     public void testParseEquationParen12() {
         Calculator instance = new Calculator();
@@ -77,7 +102,7 @@ public class CalculatorTest {
     }
 
     /**
-     * Test solveEquation with null equation.
+     * Tests solveEquation with null equation.
      *
      * @author cleber
      */
@@ -88,7 +113,7 @@ public class CalculatorTest {
     }
 
     /**
-     * Test solveEquation with empty equation.
+     * Tests solveEquation with empty equation.
      *
      * @author cleber
      */
@@ -99,7 +124,7 @@ public class CalculatorTest {
     }
 
     /**
-     * Test solveEquation with a equation filled with tab.
+     * Tests solveEquation with a equation filled with tab.
      *
      * @author cleber
      */
@@ -110,7 +135,7 @@ public class CalculatorTest {
     }
 
     /**
-     * Test solveEquation with a equation filled with tabs and spaces.
+     * Tests solveEquation with a equation filled with tabs and spaces.
      *
      * @author cleber
      */
@@ -458,126 +483,4 @@ public class CalculatorTest {
         instance.solveEquation("div 20 ! 5");
     }
     // Tests to the searchHist method and ! operand - BEGINNING
-
-//**************************Test Main Method*********************
-    /**
-     * Test of main method, of class Main.
-     *
-     * @author cleber
-     * @throws java.io.UnsupportedEncodingException
-     */
-    @Test
-    public void testMain() throws UnsupportedEncodingException {
-        sysin = new ByteArrayInputStream("quit".getBytes("UTF8"));
-        System.setIn(sysin);
-        Main main = new Main();
-        Main.main(null);
-        String message = "Please enter an equation, \n or \"cmd\" to list "
-                + "operations, \n or \"quit\" to exit\n"
-                + System.lineSeparator() + "Goodbye!" + System.lineSeparator();
-
-        assertEquals(message, sysout.toString());
-    }
-
-    /**
-     * Test of main method, of class Main.
-     *
-     * @author cleber
-     * @throws java.io.UnsupportedEncodingException
-     */
-    @Test
-    public void testPrintCommands() throws UnsupportedEncodingException {
-        sysin = new ByteArrayInputStream(("cmd" + System.lineSeparator() + "quit").getBytes("UTF8"));
-        System.setIn(sysin);
-        Main.main(null);
-        String message = "Please enter an equation, \n or \"cmd\" to list "
-                + "operations, \n or \"quit\" to exit\n"
-                + System.lineSeparator() + "Command \t Format" + System.lineSeparator()
-                + "Addition\t add 1 2 3" + System.lineSeparator()
-                + "Subtraction\t sub 1 2 3" + System.lineSeparator()
-                + "Multiplication\t mul 1 2 3" + System.lineSeparator()
-                + "Division\t div 1 2 3" + System.lineSeparator()
-                + "Inner operation\t add 1 (add 1 2) 3" + System.lineSeparator()
-                + "Show History\t hist" + System.lineSeparator()
-                + "Clear History\t clear" + System.lineSeparator()
-                + "history entry n\t add 1 !n" + System.lineSeparator()
-                + "Goodbye!" + System.lineSeparator();
-        assertEquals(message, sysout.toString());
-    }
-
-    /**
-     * Test of getInput method, of class Main.
-     *
-     * @author cleber
-     */
-    @Test
-    public void testGetInputQuit() {
-        String s = "quit";
-        Calculator c = new Calculator();
-        boolean expResult = false;
-        boolean result = Main.getInput(s, c);
-        assertEquals(expResult, result);
-
-        String message = "Goodbye!" + System.lineSeparator();
-        assertEquals(message, sysout.toString());
-    }
-
-    /**
-     * Test of getInput method, of class Main, with a null value for the
-     * <code>s</code> parameter.
-     *
-     * @author cleber
-     */
-    @Test
-    public void testGetInputNullS() {
-        String s = null;
-        Calculator c = new Calculator();
-        boolean expResult = true;
-        boolean result = Main.getInput(s, c);
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getInput method, of class Main, with a null value for the
-     * <code>c</code> parameter.
-     *
-     * @author cleber
-     */
-    @Test
-    public void testGetInputNullC() {
-        String s = "add 2 3";
-        Calculator c = null;
-        boolean expResult = true;
-        boolean result = Main.getInput(s, c);
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getInput method, of class Main, with a null value for the
-     * <code>s</code> and <code>c</code> parameters.
-     *
-     * @author cleber
-     */
-    @Test
-    public void testGetInputNullSC() {
-        String s = null;
-        Calculator c = null;
-        boolean expResult = true;
-        boolean result = Main.getInput(s, c);
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getInput method, of class Main, with valid equation.
-     *
-     * @author cleber
-     */
-    @Test
-    public void testGetInput1() {
-        String s = "add 2 3 4";
-        Calculator c = new Calculator();
-        boolean expResult = true;
-        boolean result = Main.getInput(s, c);
-        assertEquals(expResult, result);
-    }
 }
